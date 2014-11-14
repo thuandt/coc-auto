f5a5ae09d23f20fb1628d25a5d9c0d6b03c504aa
MrTux <mrtux@ubuntu-vn.org>
2014-11-15 01:37:03 +0700

    refactor webapp.py. Remove call process to ngrok program

--- coc_auto_web/webapp.py
+++ coc_auto_web/webapp.py
@@ -15,29 +15,34 @@ urls = ('/', 'Index')
 title = "Web interface for AutoCoC Tools by MrTux"
 app = web.application(urls, globals())
 
-f = open("/tmp/coc_status", 'w')
+coc_status = "/tmp/coc_status"
+kill_script = "pkill -f keepalive.py"
+keepalive_script = "python /home/mrtux/app/coc-auto/keepalive.py"
+webpy_templates = "/home/mrtux/app/coc-auto/templates/"
+
+start_button = web.form.Button('action', html="Start",
+                               class_="btn btn-success btn-lg",
+                               style="width: 5em;height: 2em;font-size: 70px",
+                               id="control_button", value="start")
+
+stop_button = web.form.Button('action', html="Stop",
+                              class_="btn btn-danger btn-lg",
+                              style="width: 5em;height: 2em;font-size: 70px",
+                              id="control_button", value="stop")
+
+f = open(coc_status, 'w')
 f.write("offline")
 f.close()
-
-ngrok_prog = subprocess.Popen("/home/mrtux/app/bin/ngrok start coc", shell=True)
 
 
 class Index:
 
     def __init__(self):
-        if open("/tmp/coc_status", 'r').readline() == "offline":
-            self.control_form_button = web.form.Button('action', html="Start",
-                                                       class_="btn btn-success btn-lg",
-                                                       style="width: 5em;height: 2em;font-size: 70px",
-                                                       id="control_button", value="start")
-        elif open("/tmp/coc_status", 'r').readline() == "running":
-            self.control_form_button = web.form.Button('action', html="Stop",
-                                                       class_="btn btn-danger btn-lg",
-                                                       style="width: 5em;height: 2em;font-size: 70px",
-                                                       id="control_button", value="stop")
-
-        self.control_form = web.form.Form(self.control_form_button)
-        self.render = web.template.render('templates/')
+        if open(coc_status, 'r').readline() == "offline":
+            self.control_form = web.form.Form(start_button)
+        elif open(coc_status, 'r').readline() == "running":
+            self.control_form = web.form.Form(stop_button)
+        self.render = web.template.render(webpy_templates)
 
     def GET(self):
         return self.render.index(title, self.control_form)
@@ -46,15 +51,15 @@ class Index:
         data = web.input()
         action = data.action
         if action == "start":
-            f = open("/tmp/coc_status", 'w')
+            f = open(coc_status, 'w')
             f.write("running")
             f.close()
-            prog = subprocess.Popen("python /home/mrtux/app/coc-auto/keepalive.py", shell=True)
+            subprocess.Popen(keepalive_script, shell=True)
         elif action == "stop":
-            file_status = open("/tmp/coc_status", 'r').readline()
+            file_status = open(coc_status, 'r').readline()
             if file_status == "running":
-                prog = subprocess.Popen("pkill -f keepalive.py", shell=True)
-                f = open("/tmp/coc_status", 'w')
+                subprocess.Popen(kill_script, shell=True)
+                f = open(coc_status, 'w')
                 f.write("offline")
                 f.close()
 
